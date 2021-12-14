56c94bf6401f5aa7de8a795d52ad212bf13a5f4e
Mateusz Okulewicz <mat@citrusad.com>
2021-12-14 20:15:49 +0100

    nebular installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NbThemeModule.forRoot({ name: 'cosmic' }), NbLayoutModule, NbEvaIconsModule],
   providers: [],
 })
 export class AppModule {}
