02c74d970ea33ce6ef5c07319da82e7c4d5eb562
Mateusz Okulewicz <mat@citrusad.com>
2021-12-21 21:19:08 +0100

    sidebar

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
-import { NbLayoutModule, NbSidebarModule } from '@nebular/theme';
+import { NbButtonModule, NbIconModule, NbLayoutModule, NbSidebarModule, NbSidebarService } from '@nebular/theme';
+import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, NbLayoutModule, NbSidebarModule],
+  imports: [CommonModule, NbLayoutModule, NbSidebarModule, NbIconModule, NbButtonModule, FlexModule],
 })
 export class LayoutModule {}
